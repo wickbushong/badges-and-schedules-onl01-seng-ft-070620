232d5cc55e322a3fad70af11a7b37cd8c59a8aa2
Wick Bushong <wickbushong@gmail.com>
2020-07-07 15:18:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,13 @@ def assign_rooms(array)
 end
 
 
-def printer
-
+def printer(array)
+  batch_badge_creator(array)
+  assign_rooms(array)
+  batch.each {|badge|
+    puts badge
+  }
+  ans.each {|assignment|
+    puts assignment
+  }
 end
